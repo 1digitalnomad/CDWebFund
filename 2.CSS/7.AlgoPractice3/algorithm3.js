4d6fcdd1a95f2d4d19f834cf20834373ae99db52
Raul <Takethefirststep@Rauls-iMac.attlocal.net>
2018-08-11 22:44:46 -0700

    Here are my assignments as we continue on our journey.

---
+++ 2.CSS/7.AlgoPractice3/algorithm3.js
@@ -0,0 +1,25 @@
+// this is part 2 of the assignment. Copy/paste the starter code from the platform like seen below 
+//and edit the code to make it work.
+function printUpTo(x){
+    // your code here
+}
+printUpTo(1000000); // should print all the integers from 1 to 1000000
+y = printUpTo(-10); // should return false
+console.log(y); // should print false
+
+
+
+
+
+function printAverage(x){
+    sum = 0;
+   for(i=0; i<x.lenght; i++)
+   if(i)
+   
+   return x[i] // your code here
+ }
+ y = printAverage([1,2,3]);
+ console.log(y); // should log 2
+   
+ y = printAverage([2,5,8]);
+ console.log(y); // should log 5
