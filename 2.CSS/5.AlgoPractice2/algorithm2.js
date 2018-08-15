7515ddfa13ee74add85efa4d83755a39473b5a85
Raul <Takethefirststep@Rauls-iMac.local>
2018-08-15 00:31:28 -0700

    pushing my algo practice2 results

--- 2.CSS/5.AlgoPractice2/algorithm2.js
+++ 2.CSS/5.AlgoPractice2/algorithm2.js
@@ -1,27 +1,30 @@
 // this is part 2 of the assignment. Copy/paste the starter code from the platform like seen below 
 //and edit the code to make it work.
 function printUpTo(x){
-    for(var i=1; i<=x; i++)
-    console.log(i);
-    // your code here
-}
-printUpTo(1000000); // should print all the integers from 1 to 1000000
-y = printUpTo(-10); // should return false
-console.log(y); // should print false
+  if(x<0)
+    return false;
+   
+   for(i=1; i<=x; i++){
+    console.log(i)
+  }
+ }
+ printUpTo(50); // should print all the integers from 1 to 1000000
+ y = printUpTo(-10); // should return false
+ console.log(y); // should print false
 
 // print sum
 
 function printSum(x){
-    var sum = 0;
-   for(var i=0; i<=255; i++)
-          
-        console.log(i+1)
+  var sum = 0;
+ for(var i=0; i<=x; i++){
+           sum=sum+i;
+   console.log(i+1)
+}
+  return sum;
 
-    //your code here
-    return sum
-  }
-  y = printSum(255) // should print all the integers from 0 to 255 and with each integer print the sum so far.
-  console.log(y) // should print 32640
+}
+  y = printSum(255); // should print all the integers from 0 to 255 and with each integer print the sum so far.
+  console.log(y); // should print 32640
 
 
   //printSumArray
@@ -29,7 +32,9 @@ function printSum(x){
   function printSumArray(x){
     var sum = 0;
     for(var i=0; i<x.length; i++) {
-      //your code here
+       sum=sum+i;}
+      for(var i=0; i<x.length; i++){
+        sum=sum+i;
     }
     return sum;
   }
--- 2.CSS/7.AlgoPractice3/algorithm3.js
+++ 2.CSS/7.AlgoPractice3/algorithm3.js
@@ -9,17 +9,54 @@ console.log(y); // should print false
 
 
 
+//1) Analyze an array’s values and return the average of its values.
 
 
-function printAverage(x){
-    sum = 0;
-   for(i=0; i<x.lenght; i++)
-   if(i)
+function printaverage(array){
+    var sum=0
+    for(var i=0; i<array.length; i++){
+        console.log(i);
+        
+
+    }
+}
+printaverage([1,4,5]);
+
+// function printAverage(x){
+//     sum = 0;
+
+
+//     // your code here
+//  }
+//  y = printAverage([1,2,3]);
+//  console.log(y); // should log 2
    
-   return x[i] // your code here
- }
- y = printAverage([1,2,3]);
- console.log(y); // should log 2
+//  y = printAverage([2,5,8]);
+//  console.log(y); // should log 5
+
+
+ //Create an array with all the odd integers between 1 and 255 (inclusive)
+
+
+
+
+ 
+//  function returnOddArray(){
+//     for(i=1; )
+//     // your code here
+//  }
+//  y = returnOddArray();
+//  console.log(y); // should log [1,3,5,...,253,255]
+
+
+//  //3) Square each value in a given array, returning that same array with changed values.
+
+//  function squareValue(x){
+//     // your code here
+//     return x;
+//  }
+//  y = squareValue([1,2,3]);
+//  console.log(y); // should log [1,4,9]
    
- y = printAverage([2,5,8]);
- console.log(y); // should log 5
+//  y = squareValue([2,5,8]);
+//  console.log(y); // should log [4,25,64]
